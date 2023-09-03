cc38bd96015808071aa352be0fb3b7b08bc3d5c8
Muffiop52 <99004287+Muffiop52@users.noreply.github.com>
2023-09-03 08:58:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function addUser()
+{
+    player1_name = document.getElementById("player1_username").value;
+    player2_name = document.getElementById("player2_username").value;
+    
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+
+    window.location = "game_index.html";
+}
